d5c637155dcd4484ee76f0f226de85cb5dcb07df
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-25 20:45:09 -0300

    Changes of types

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,8 +7,8 @@ import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
-class Dashboard extends Component {
-  constructor(props) {
+export function Dashboard() {
+  /*constructor(props) {
     super(props);
     this.state = {
       foods: [],
@@ -16,15 +16,15 @@ class Dashboard extends Component {
       modalOpen: false,
       editModalOpen: false,
     }
-  }
+  }*/
 
-  async componentDidMount() {
+  async function componentDidMount() {
     const response = await api.get('/foods');
 
     this.setState({ foods: response.data });
   }
 
-  handleAddFood = async food => {
+  async function handleAddFood(food) {
     const { foods } = this.state;
 
     try {
@@ -39,18 +39,13 @@ class Dashboard extends Component {
     }
   }
 
-  handleUpdateFood = async food => {
+  async function handleUpdateFood(food) {
     const { foods, editingFood } = this.state;
 
     try {
-      const foodUpdated = await api.put(
-        `/foods/${editingFood.id}`,
-        { ...editingFood, ...food },
-      );
+      const foodUpdated = await api.put(`/foods/${editingFood.id}`, { ...editingFood, ...food });
 
-      const foodsUpdated = foods.map(f =>
-        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
-      );
+      const foodsUpdated = foods.map((f) => (f.id !== foodUpdated.data.id ? f : foodUpdated.data));
 
       this.setState({ foods: foodsUpdated });
     } catch (err) {
@@ -58,64 +53,53 @@ class Dashboard extends Component {
     }
   }
 
-  handleDeleteFood = async id => {
+  async function handleDeleteFood(id) {
     const { foods } = this.state;
 
     await api.delete(`/foods/${id}`);
 
-    const foodsFiltered = foods.filter(food => food.id !== id);
+    const foodsFiltered = foods.filter((food) => food.id !== id);
 
     this.setState({ foods: foodsFiltered });
   }
 
-  toggleModal = () => {
+  function toggleModal() {
     const { modalOpen } = this.state;
 
     this.setState({ modalOpen: !modalOpen });
   }
 
-  toggleEditModal = () => {
+  function toggleEditModal() {
     const { editModalOpen } = this.state;
 
     this.setState({ editModalOpen: !editModalOpen });
   }
 
-  handleEditFood = food => {
+  function handleEditFood(food) {
     this.setState({ editingFood: food, editModalOpen: true });
   }
 
-  render() {
-    const { modalOpen, editModalOpen, editingFood, foods } = this.state;
+  const { modalOpen, editModalOpen, editingFood, foods } = this.state;
 
-    return (
-      <>
-        <Header openModal={this.toggleModal} />
-        <ModalAddFood
-          isOpen={modalOpen}
-          setIsOpen={this.toggleModal}
-          handleAddFood={this.handleAddFood}
-        />
-        <ModalEditFood
-          isOpen={editModalOpen}
-          setIsOpen={this.toggleEditModal}
-          editingFood={editingFood}
-          handleUpdateFood={this.handleUpdateFood}
-        />
+  return (
+    <>
+      <Header openModal={this.toggleModal} />
+      <ModalAddFood isOpen={modalOpen} setIsOpen={toggleModal} handleAddFood={handleAddFood} />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
 
-        <FoodsContainer data-testid="foods-list">
-          {foods &&
-            foods.map(food => (
-              <Food
-                key={food.id}
-                food={food}
-                handleDelete={this.handleDeleteFood}
-                handleEditFood={this.handleEditFood}
-              />
-            ))}
-        </FoodsContainer>
-      </>
-    );
-  }
-};
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food key={food.id} food={food} handleDelete={handleDeleteFood} handleEditFood={handleEditFood} />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
 
 export default Dashboard;
